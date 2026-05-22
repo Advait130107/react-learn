0f00c0696292d86cd23c85eb82c81236e707f18d
Advait130107 <advaituttekar1301@gmail.com>
2026-05-22 15:16:06 +0530

    Add Card component and update App to use it; change name color in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,57 @@
 import "./App.css";
 import "./index.css";
 
+import Card from "./Card";
 function App() {
   return (
     <>
       <div className="header">
         <h3>About Our Experts</h3>
+
         <p>
-          {" "}
           Meet our talented professionals who help us create amazing
           experiences.
         </p>
       </div>
+
       <div className="cardcollection">
-        <div className="card1">
-          <div className="container">
-            <img
-              className="img1"
-              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
-              alt="img 1"
-            />
-          </div>
-          <div className="aboutimg1">
-            <h2 className="name">Name</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
-            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
-            dolorum praesentium? Voluptate blanditiis inventore ea, animi
-            adipisci iusto!
-            <button className="button">Learn More</button>
-          </div>
-        </div>
-        <div className="card1">
-          <div className="container">
-            <img
-              className="img1"
-              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
-              alt="img 1"
-            />
-          </div>
-          <div className="aboutimg1">
-            <h2 className="name">Name</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
-            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
-            dolorum praesentium? Voluptate blanditiis inventore ea, animi
-            adipisci iusto!
-            <button className="button">Learn More</button>
-          </div>
-        </div>
+        <Card
+          name="name"
+          image="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+          description="lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, cumque."
+        />
+        <Card
+          name="name"
+          image="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+          description="lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, cumque."
+        />
 
-        <div className="card1">
-          <div className="container">
-            <img
-              className="img1"
-              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
-              alt="img 1"
-            />
-          </div>
-          <div className="aboutimg1">
-            <h2 className="name">Name</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
-            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
-            dolorum praesentium? Voluptate blanditiis inventore ea, animi
-            adipisci iusto!
-            <button className="button">Learn More</button>
-          </div>
-        </div>
-        <div className="card1">
-          <div className="container">
-            <img
-              className="img1"
-              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
-              alt="img 1"
-            />
-          </div>
-          <div className="aboutimg1">
-            <h2 className="name">Name</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
-            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
-            dolorum praesentium? Voluptate blanditiis inventore ea, animi
-            adipisci iusto!
-            <button className="button">Learn More</button>
-          </div>
-        </div>
+        <Card
+          name="name"
+          image="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+          description="lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, cumque."
+        />
 
-        <div className="card1">
-          <div className="container">
-            <img
-              className="img1"
-              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
-              alt="img 1"
-            />
-          </div>
-          <div className="aboutimg1">
-            <h2 className="name">Name</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
-            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
-            dolorum praesentium? Voluptate blanditiis inventore ea, animi
-            adipisci iusto!
-            <button className="button">Learn More</button>
-          </div>
-        </div>
-        <div className="card1">
-          <div className="container">
-            <img
-              className="img1"
-              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
-              alt="img 1"
-            />
-          </div>
-          <div className="aboutimg1">
-            <h2 className="name">Name</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
-            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
-            dolorum praesentium? Voluptate blanditiis inventore ea, animi
-            adipisci iusto!
-            <button className="button">Learn More</button>
-          </div>
-        </div>
+        <Card
+          name="name"
+          image="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+          description="lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, cumque."
+        />
+
+        <Card
+          name="name"
+          image="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+          description="lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, cumque."
+        />
+
+        <Card
+          name="name"
+          image="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+          description="lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, cumque."
+        />
       </div>
     </>
   );
 }
+
 export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,19 @@
+function Card({ name, image, description }) {
+  return (
+    <div className="card1">
+      <div className="container">
+        <img className="img1" src={image} alt={name} />
+      </div>
+
+      <div className="aboutimg1">
+        <h2 className="name">{name}</h2>
+
+        <p>{description}</p>
+
+        <button className="button">Learn More</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
