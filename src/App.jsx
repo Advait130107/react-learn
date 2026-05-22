a6b2502d944dc92ed0781b4e178f426f24dc1e6a
Advait130107 <advaituttekar1301@gmail.com>
2026-05-22 13:35:44 +0530

    Add initial project setup with components, styles, and assets
    - Created main application structure in App.jsx with header and card components.
    - Added CSS styles for layout and design in App.css.
    - Included SVG icons for various functionalities in icons.svg.
    - Added hero image and React/Vite logos in assets.
    - Set up Vite configuration for the project.
    - Implemented main entry point in main.jsx to render the application.

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <h3> About our Expert</h3>
+        <p> </p>
+      </div>
+      <div className="cardcollection">
+        <div className="card1">
+          <div className="container">
+            <img
+              className="img1"
+              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+              alt="img 1"
+            />
+          </div>
+          <div className="aboutimg1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
+            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
+            dolorum praesentium? Voluptate blanditiis inventore ea, animi
+            adipisci iusto!
+            <button className="button">Learn More</button>
+          </div>
+        </div>
+        <div className="card1">
+          <div className="container">
+            <img
+              className="img1"
+              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+              alt="img 1"
+            />
+          </div>
+          <div className="aboutimg1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
+            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
+            dolorum praesentium? Voluptate blanditiis inventore ea, animi
+            adipisci iusto!
+            <button className="button">Learn More</button>
+          </div>
+        </div>
+      </div>
+      <div className="cardcollection">
+        <div className="card1">
+          <div className="container">
+            <img
+              className="img1"
+              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+              alt="img 1"
+            />
+          </div>
+          <div className="aboutimg1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
+            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
+            dolorum praesentium? Voluptate blanditiis inventore ea, animi
+            adipisci iusto!
+            <button className="button">Learn More</button>
+          </div>
+        </div>
+        <div className="card1">
+          <div className="container">
+            <img
+              className="img1"
+              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+              alt="img 1"
+            />
+          </div>
+          <div className="aboutimg1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
+            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
+            dolorum praesentium? Voluptate blanditiis inventore ea, animi
+            adipisci iusto!
+            <button className="button">Learn More</button>
+          </div>
+        </div>
+      </div>
+      <div className="cardcollection">
+        <div className="card1">
+          <div className="container">
+            <img
+              className="img1"
+              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+              alt="img 1"
+            />
+          </div>
+          <div className="aboutimg1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
+            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
+            dolorum praesentium? Voluptate blanditiis inventore ea, animi
+            adipisci iusto!
+            <button className="button">Learn More</button>
+          </div>
+        </div>
+        <div className="card1">
+          <div className="container">
+            <img
+              className="img1"
+              src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Picture.png"
+              alt="img 1"
+            />
+          </div>
+          <div className="aboutimg1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto totam
+            illum eaque soluta dolore. Nulla suscipit fugit ut sit ex itaque
+            dolorum praesentium? Voluptate blanditiis inventore ea, animi
+            adipisci iusto!
+            <button className="button">Learn More</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
