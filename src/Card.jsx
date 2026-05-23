cc09d75871964544dd397ed10c86d2c4bb5460c8
Advait130107 <advaituttekar1301@gmail.com>
2026-05-23 13:10:18 +0530

    refactor: update button styles and improve Card component functionality

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,7 @@ function Card({ name, image, description, moreinfo }) {
   const [showMore, setShowMore] = useState(false);
 
   return (
-    <div className={`card1 ${showMore ? "expandCard" : ""}`}>
+    <div className={`card1 ${showMore ? "moreInfo" : ""}`}>
       <div className="container">
         <img
           className="img1"
@@ -18,18 +18,22 @@ function Card({ name, image, description, moreinfo }) {
         <h2 className="name">{name}</h2>
 
         <p>{description}</p>
-
         {showMore && (
           <div className="moreInfo">
             <p>{moreinfo}</p>
           </div>
         )}
 
-        <button className="button" onClick={() => setShowMore(true)}>
-          Learn More
-        </button>
-
-        <button onClick={() => setShowMore(false)}>Show Less</button>
+        {!showMore && (
+          <button className="button" onClick={() => setShowMore(true)}>
+            Learn More
+          </button>
+        )}
+        {showMore && (
+          <button className="lessinfobutton" onClick={() => setShowMore(false)}>
+            Show Less
+          </button>
+        )}
       </div>
     </div>
   );
